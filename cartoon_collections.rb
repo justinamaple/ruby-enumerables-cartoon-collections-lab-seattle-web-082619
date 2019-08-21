d7c686f097d765f919d714050db06b5b50eb130d
Justin Maple <justinamaple@gmail.com>
2019-08-21 07:19:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,9 @@ def find_the_cheese(items)
   cheese_types = ["cheddar", "gouda", "camembert"]
   
   items.each do |item|
-    cheese_types.include(item)
+    if(cheese_types.include(item))
+      return item
   end
+  
+  nil
 end
